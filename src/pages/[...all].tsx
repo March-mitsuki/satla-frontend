38337c272bf13328882fbbadb8074f6682ac08ff
March-mitsuki <march_tsuki@icloud.com>
2022-09-13 08:28:15 +0900

    [update] add back btn

--- src/pages/[...all].tsx
+++ src/pages/[...all].tsx
@@ -1,8 +1,11 @@
 const NotFound = () => {
   return (
-    <div>
-      404 Not Found
-    </div>
+    <>
+      <div>
+        404 Not Found
+      </div>
+      <a href="/">Back to home</a>
+    </>
   )
 }
 
