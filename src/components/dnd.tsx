2a05ef4e3f78bde468208eb1523d4b8d4116a76b
March-mitsuki <march_tsuki@icloud.com>
2022-09-13 11:49:57 +0900

    [add] drag and drop template

---
+++ src/components/dnd.tsx
@@ -0,0 +1,96 @@
+import { createSignal, onMount } from "solid-js";
+
+const Dragpage = () => {
+  const [mouseCoordinates, setMouseCoordinates] = createSignal({
+    x: 0,
+    y: 0,
+  })
+  const [dragElem, setDragElem] = createSignal<{
+    zIndex: number | "auto",
+    position: "static" | "relative" | "absolute" | "sticky" | "fixed",
+    hidden: boolean,
+  }>({
+    zIndex: "auto",
+    position: "static",
+    hidden: false,
+  })
+
+  let dragElemRef: HTMLDivElement | undefined;
+  let dropElemRef: HTMLDivElement | undefined;
+
+  let startDragHandler = (e: MouseEvent) => {
+    let belowElem: Element | null
+
+    onmousemove = (e: MouseEvent) => {
+      e.preventDefault()
+      setDragElem({
+        zIndex: 1000,
+        position: "absolute",
+        hidden: true,
+      })
+      belowElem = document.elementFromPoint(mouseCoordinates().x, mouseCoordinates().y)
+      setDragElem({
+        zIndex: 1000,
+        position: "absolute",
+        hidden: false,
+      })
+      setMouseCoordinates({
+        x: e.clientX,
+        y: e.clientY,
+      })
+      if (belowElem) {
+        if (belowElem === dropElemRef) {
+          console.log("drag enter");
+        }
+      }
+    }
+
+    onmouseup = (e: MouseEvent) => {
+      setDragElem({
+        zIndex: "auto",
+        position: "static",
+        hidden: false,
+      })
+      if (belowElem) {
+        if (belowElem === dropElemRef) {
+          console.log("drop in");
+        }
+      }
+      onmousemove = () => null
+      onmouseup = () => null
+    }
+  }
+
+  onMount(() => {
+
+  })
+
+  return (
+    <>
+      <div
+        ref={dragElemRef}
+        style={{
+          "z-index": `${dragElem().zIndex}`,
+          "position": `${dragElem().position}`,
+          "left": `${dragElemRef ? mouseCoordinates().x - dragElemRef.offsetWidth/2 : ""}px`,
+          "top": `${dragElemRef ? mouseCoordinates().y - dragElemRef.offsetHeight/2 : ""}px`
+        }}
+        onMouseDown={startDragHandler}
+        hidden={dragElem().hidden}
+      >
+        Drag me
+      </div>
+      <div
+        ref={dropElemRef}
+        style={{
+          "height": "300px",
+          "background-color": "rgba(120, 230, 60, 0.2)",
+        }}
+      >
+        drop here
+      </div>
+    </>
+  )
+}
+
+export default Dragpage
