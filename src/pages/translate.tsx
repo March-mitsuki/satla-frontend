1844a8bfaec37c2c1bbb12e4e6ef70aee619cd31
March-mitsuki <march_tsuki@icloud.com>
2022-09-14 21:49:37 +0900

    [update] translate page layout finish

--- src/pages/translate.tsx
+++ src/pages/translate.tsx
@@ -1,7 +1,7 @@
 import { VideoJS } from "@/components"
 import videojs from "video.js"
 
-import { FloatingWindowX } from "@/components";
+import { FloatingWindowX, FloatingWindow } from "@/components";
 
 
 const TranslatePage = () => {
@@ -18,30 +18,65 @@ const TranslatePage = () => {
 
   return (
     <>
-      <FloatingWindowX
-        defaultWindowSize={{
-          width: 200,
-        }}
-        wrapperClass=""
-        controllerWrapperClass="flex justify-between items-center border-2 rounded-lg mb-1 bg-red-200"
-        floatingControlClass="w-fit"
-        floatingControlContent={
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
-          </svg>
-        }
-        floatingContent={
-          <div>VideoSource</div>
-        }
-        cancelControlContent={
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        }
-        contentsWrapperClass="border-2 rounded-lg bg-red-200"
-      >
-        <VideoJS {...videoJSOption}></VideoJS>
-      </FloatingWindowX>
+      <div class="h-full flex flex-col">
+        <div class="bg-sky-100">Tool Bar</div>
+        <div class="flex flex-auto">
+          <div class="flex flex-col">
+            <FloatingWindowX
+              defaultWindowSize={{
+                width: 400,
+              }}
+              wrapperClass="w-[400px] flex-initial"
+              controllerWrapperClass="flex justify-between items-center border-2 rounded-lg mb-1 bg-red-200"
+              floatingControlContent={
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+                </svg>
+              }
+              floatingContent={
+                <div>直播源</div>
+              }
+              cancelControlContent={
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              }
+              contentsWrapperClass="border-2 rounded-lg bg-red-200"
+            >
+              <VideoJS {...videoJSOption}></VideoJS>
+            </FloatingWindowX>
+            <FloatingWindow
+              defaultWindowSize={{
+                width: 400,
+                height: 200,
+              }}
+              wrapperClass="w-[400px] flex flex-col flex-auto"
+              controllerWrapperClass="flex justify-between items-center border-2 rounded-lg mb-1 bg-red-200"
+              floatingControlContent={
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+                </svg>
+              }
+              floatingContent={
+                <div>翻译输入区</div>
+              }
+              cancelControlContent={
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              }
+              contentsWrapperClass="border-2 rounded-lg bg-red-200 flex-auto"
+            >
+              <div>
+                InputForm Here
+              </div>
+            </FloatingWindow>
+          </div>
+          <div class="bg-neutral-300 w-full">
+            Subtitles Here
+          </div>
+        </div>
+      </div>
     </>
   )
 }
