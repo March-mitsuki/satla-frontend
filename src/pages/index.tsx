d71f53cccb2d2f487aa3272aac4a4852d4c296e1
March-mitsuki <march_tsuki@icloud.com>
2022-09-15 12:11:42 +0900

    [update] index layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,26 @@
 import type { Component } from 'solid-js';
 import { Title } from '@solidjs/meta';
 
+import { PaneX } from '@/components';
+
 const Home: Component = () => {
   return (
     <>
       <Title>Vvvorld</Title>
       <div class="h-full flex flex-col">
         <div class="bg-sky-100">Tool Bar</div>
-        <div class="flex flex-auto">
-          <div class="flex flex-col">
-            <div>Someting here</div>
-          </div>
-          <div class="bg-neutral-300 w-full">
-            Subtitles Here
-          </div>
-        </div>
+        <PaneX
+          leftElem={
+            <div>
+              Project here
+            </div>
+          }
+          rightElem={
+            <div>
+              Details here
+            </div>
+          }
+        ></PaneX>
       </div>
     </>
   );
