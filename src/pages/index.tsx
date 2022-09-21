296e82f85e4b3403e40639268a60739151400e55
March-mitsuki <march_tsuki@icloud.com>
2022-09-22 05:11:11 +0900

    [update] index layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,33 +19,41 @@ const Home: Component = () => {
         </div> */}
         <div class="flex-auto">
           <PaneX
-            minLeftElem="30%"
+            minLeftElem="35%"
             minRightElem="30%"
             leftElem={
               <div class="p-2">
                 <div class="flex flex-col gap-2 border-2 p-2">
-                  <div class="col-span-full grid grid-cols-5 gap-2 justify-between">
+                  <div class="col-span-full grid grid-cols-6 gap-2 justify-between">
                     <div class="text-center">ID</div>
                     <div class="text-center">项目名称</div>
                     <div class="text-center">项目描述</div>
-                    <div class="text-center col-span-2">操作</div>
+                    <div class="text-center col-span-3">操作</div>
                   </div>
                   <For each={dummyProject}>{(elem, idx) => 
-                    <div class="col-span-full grid grid-cols-5 gap-2">
+                    <div class="col-span-full grid grid-cols-6 gap-2">
                       <div class="text-center truncate">{elem.id}</div>
                       <div class="text-center truncate">{elem.project_name}</div>
                       <div class="text-center truncate">{elem.description}</div>
                       <div class="text-center">
                         <Link
                           href={`/translate/${elem.project_name}`}
-                          class="bg-neutral-500 rounded-md text-center truncate px-1 py-[1px] hover:bg-neutral-600"
+                          class="bg-neutral-500 rounded-md text-center truncate px-1 py-[3px] hover:bg-neutral-600"
                         >同传页面</Link>
                       </div>
                       <div class="text-center">
                         <Link
                           href={`/send/${elem.project_name}`}
-                          class="bg-neutral-500 rounded-md text-center truncate px-2 py-[1px] hover:bg-neutral-600"
+                          class="bg-neutral-500 rounded-md text-center truncate px-1 py-[3px] hover:bg-neutral-600"
                         >发送页面</Link>
+                      </div>
+                      <div class="text-center">
+                        <button
+                          onClick={() => console.log("查看详情 ->", elem.project_name)}
+                          class="bg-neutral-500 rounded-md text-center truncate px-1 hover:bg-neutral-600"
+                        >
+                          查看详情
+                        </button>
                       </div>
                     </div>
                   }</For>
