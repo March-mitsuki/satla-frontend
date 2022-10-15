3dc2366c9aaa55438a3ec7f7acd8b626eeb74fd8
March-mitsuki <march_tsuki@icloud.com>
2022-10-15 15:09:14 +0900

    [fix] send-area没字幕时不报错

--- src/components/pages/send-area.tsx
+++ src/components/pages/send-area.tsx
@@ -296,135 +296,139 @@ const SendArea: ParentComponent<{
       <For each={subtitles()}>{(elem, idx) => {
         console.log("creat For!");
         return (
-          <div
-            style={{
-              "z-index": (attachedInfo() as AttachedInfo[])[idx()].zIndex,
-              "position": `${(attachedInfo() as AttachedInfo[])[idx()].position}`,
-              "top": `${(attachedInfo() as AttachedInfo[])[idx()].y}px`,
-            }}
-            classList={{
-              "mt-2": (attachedInfo() as AttachedInfo[])[idx()].isDrop === false,
-              "mt-2 border-t-2 border-sky-500": (attachedInfo() as AttachedInfo[])[idx()].isDrop === true,
-            }}
-            hidden={(attachedInfo() as AttachedInfo[])[idx()].hidden}
-          >
-            <form
-              id={`${elem.id}-form`}
-              onKeyDown={(e) => formKeyDownHander(e, idx(), elem)}
-              onSubmit={(e) => onSubmitHandler(e, idx(), elem)}
-              onInput={() => onInputHandler(idx())}
-              classList={{
-                "flex px-2 gap-2 items-center": (attachedInfo() as AttachedInfo[])[idx()].changeStatus === 0,
-                "flex px-2 gap-2 items-center border-2 border-sky-500 rounded-lg": (attachedInfo() as AttachedInfo[])[idx()].changeStatus === 1,
-                "flex px-2 gap-2 items-center border-2 border-red-500 rounded-lg": (attachedInfo() as AttachedInfo[])[idx()].changeStatus === 2,
-              }}
-            >
-              <Switch fallback={
-                <div
-                  class="flex gap-3 w-[180px] items-center px-1 rounded-md bg-orange-500/70 select-none"
+          <>
+            {(subtitles()?.length !== 0 && attachedInfo()?.length !== 0) &&
+              <div
+                style={{
+                  "z-index": (attachedInfo() as AttachedInfo[])[idx()].zIndex,
+                  "position": `${(attachedInfo() as AttachedInfo[])[idx()].position}`,
+                  "top": `${(attachedInfo() as AttachedInfo[])[idx()].y}px`,
+                }}
+                classList={{
+                  "mt-2": (attachedInfo() as AttachedInfo[])[idx()].isDrop === false,
+                  "mt-2 border-t-2 border-sky-500": (attachedInfo() as AttachedInfo[])[idx()].isDrop === true,
+                }}
+                hidden={(attachedInfo() as AttachedInfo[])[idx()].hidden}
+              >
+                <form
+                  id={`${elem.id}-form`}
+                  onKeyDown={(e) => formKeyDownHander(e, idx(), elem)}
+                  onSubmit={(e) => onSubmitHandler(e, idx(), elem)}
+                  onInput={() => onInputHandler(idx())}
+                  classList={{
+                    "flex px-2 gap-2 items-center": (attachedInfo() as AttachedInfo[])[idx()].changeStatus === 0,
+                    "flex px-2 gap-2 items-center border-2 border-sky-500 rounded-lg": (attachedInfo() as AttachedInfo[])[idx()].changeStatus === 1,
+                    "flex px-2 gap-2 items-center border-2 border-red-500 rounded-lg": (attachedInfo() as AttachedInfo[])[idx()].changeStatus === 2,
+                  }}
                 >
-                  <div class="flex-1">
-                    {elem.input_time}
-                  </div>
-                  <div class="flex-1 truncate text-center">
-                    {elem.translated_by}
-                  </div>
-                </div>
-              }>
-                <Match when={(attachedInfo() as AttachedInfo[])[idx()].isEditing}>
-                  <div
-                    class="flex gap-3 w-[180px] items-center px-1 rounded-md bg-red-500/70 select-none"
-                  >
-                    <div class="flex gap-1 justify-between items-center flex-1 truncate">
+                  <Switch fallback={
+                    <div
+                      class="flex gap-3 w-[180px] items-center px-1 rounded-md bg-orange-500/70 select-none"
+                    >
                       <div class="flex-1">
-                        <div class="shrink-0 animate-spin h-4 w-4 border-2 border-white rounded-full border-t-transparent"></div>
+                        {elem.input_time}
                       </div>
-                      <div class="flex-1">{(attachedInfo() as AttachedInfo[])[idx()].editingUser}</div>
+                      <div class="flex-1 truncate text-center">
+                        {elem.translated_by}
+                      </div>
                     </div>
-                  </div>
-                </Match>
-                <Match when={elem.checked_by !== null && elem.checked_by !== ""}>
-                  <div
-                    class="flex gap-3 w-[180px] items-center px-1 rounded-md bg-green-500/70 select-none"
+                  }>
+                    <Match when={(attachedInfo() as AttachedInfo[])[idx()].isEditing}>
+                      <div
+                        class="flex gap-3 w-[180px] items-center px-1 rounded-md bg-red-500/70 select-none"
+                      >
+                        <div class="flex gap-1 justify-between items-center flex-1 truncate">
+                          <div class="flex-1">
+                            <div class="shrink-0 animate-spin h-4 w-4 border-2 border-white rounded-full border-t-transparent"></div>
+                          </div>
+                          <div class="flex-1">{(attachedInfo() as AttachedInfo[])[idx()].editingUser}</div>
+                        </div>
+                      </div>
+                    </Match>
+                    <Match when={elem.checked_by !== null && elem.checked_by !== ""}>
+                      <div
+                        class="flex gap-3 w-[180px] items-center px-1 rounded-md bg-green-500/70 select-none"
+                      >
+                        <div class="flex-1">
+                          {elem.input_time}
+                        </div>
+                        <div class="flex-1 truncate text-center">
+                          {elem.checked_by}
+                        </div>
+                      </div>
+                    </Match>
+                  </Switch>
+                  <button
+                    onClick={(e) => delClickHandler(e, idx(), elem)}
+                    class="rounded-md p-1 bg-red-500/70 hover:bg-red-700/70 active:bg-red-500/70"
                   >
-                    <div class="flex-1">
-                      {elem.input_time}
-                    </div>
-                    <div class="flex-1 truncate text-center">
-                      {elem.checked_by}
-                    </div>
-                  </div>
-                </Match>
-              </Switch>
-              <button
-                onClick={(e) => delClickHandler(e, idx(), elem)}
-                class="rounded-md p-1 bg-red-500/70 hover:bg-red-700/70 active:bg-red-500/70"
-              >
-                {/* del btn */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                </svg>
-              </button>
-              <button
-                onClick={(e) => addUpClickHandler(e, idx(), elem)}
-                class="rounded-md p-1 bg-amber-500/70 hover:bg-amber-700/70 active:bg-amber-500/70"
-              >
-                {/* add up btn */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
-                </svg>
-              </button>
-              <button
-                onClick={(e) => addDownClickHandler(e, idx(), elem)}
-                class="rounded-md p-1 bg-amber-500/70 hover:bg-amber-700/70 active:bg-amber-500/70"
-              >
-                {/* add down btn */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
-                </svg>
-              </button>
-              <button
-                onClick={(e) => sendBlankHandler(e)}
-                class="rounded-md p-1 bg-sky-500/70 hover:bg-sky-700/70 active:bg-sky-500/70"
-              >
-                {/* submit btn */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                </svg>
-              </button>
-              <button
-                type="submit"
-                class="rounded-md p-1 bg-green-500/70 hover:bg-green-700/70 active:bg-green-500/70"
-              >
-                {/* submit btn */}
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                </svg>
-              </button>
-              <input
-                id={idx() + "-sub"}
-                type="text"
-                name="subtitle"
-                autocomplete="off"
-                placeholder="请输入翻译"
-                onfocus={() => wsSend.editStart(props.ws, elem.id)}
-                onBlur={() => wsSend.editEnd(props.ws, elem.id)}
-                value={elem.subtitle}
-                class={inputStyle}
-              />
-              <input
-                id={idx() + "-ori"}
-                type="text"
-                name="origin"
-                autocomplete="off"
-                placeholder="请输入原文"
-                onfocus={() => wsSend.editStart(props.ws, elem.id)}
-                onBlur={() => wsSend.editEnd(props.ws, elem.id)}
-                value={elem.origin}
-                class={inputStyle}
-              />
-            </form>
-          </div>
+                    {/* del btn */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                  </button>
+                  <button
+                    onClick={(e) => addUpClickHandler(e, idx(), elem)}
+                    class="rounded-md p-1 bg-amber-500/70 hover:bg-amber-700/70 active:bg-amber-500/70"
+                  >
+                    {/* add up btn */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
+                    </svg>
+                  </button>
+                  <button
+                    onClick={(e) => addDownClickHandler(e, idx(), elem)}
+                    class="rounded-md p-1 bg-amber-500/70 hover:bg-amber-700/70 active:bg-amber-500/70"
+                  >
+                    {/* add down btn */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
+                    </svg>
+                  </button>
+                  <button
+                    onClick={(e) => sendBlankHandler(e)}
+                    class="rounded-md p-1 bg-sky-500/70 hover:bg-sky-700/70 active:bg-sky-500/70"
+                  >
+                    {/* submit btn */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                    </svg>
+                  </button>
+                  <button
+                    type="submit"
+                    class="rounded-md p-1 bg-green-500/70 hover:bg-green-700/70 active:bg-green-500/70"
+                  >
+                    {/* submit btn */}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                    </svg>
+                  </button>
+                  <input
+                    id={idx() + "-sub"}
+                    type="text"
+                    name="subtitle"
+                    autocomplete="off"
+                    placeholder="请输入翻译"
+                    onfocus={() => wsSend.editStart(props.ws, elem.id)}
+                    onBlur={() => wsSend.editEnd(props.ws, elem.id)}
+                    value={elem.subtitle}
+                    class={inputStyle}
+                  />
+                  <input
+                    id={idx() + "-ori"}
+                    type="text"
+                    name="origin"
+                    autocomplete="off"
+                    placeholder="请输入原文"
+                    onfocus={() => wsSend.editStart(props.ws, elem.id)}
+                    onBlur={() => wsSend.editEnd(props.ws, elem.id)}
+                    value={elem.origin}
+                    class={inputStyle}
+                  />
+                </form>
+              </div>
+            }
+          </>
         )}}</For>
     </div>
   )
